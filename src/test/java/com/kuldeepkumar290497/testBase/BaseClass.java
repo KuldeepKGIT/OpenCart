37db9a1a25d5bcfb3870e7d49e05811455def61e
KuldeepKGIT <kuldeepkumar290497@gmail.com>
2025-10-05 17:52:26 +0530

    5th OCT

--- src/test/java/com/kuldeepkumar290497/testBase/BaseClass.java
+++ src/test/java/com/kuldeepkumar290497/testBase/BaseClass.java
@@ -4,18 +4,23 @@ import org.apache.commons.lang3.RandomStringUtils;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.*;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 import java.util.Properties;
 
 public class BaseClass {
-    public WebDriver driver;
+    public static WebDriver driver;
     public static Logger logger;
     public Properties p;
 
@@ -69,5 +74,21 @@ public class BaseClass {
         String str = RandomStringUtils.random(3,true, false);
         String num = RandomStringUtils.random(3,false, true);
         return str+"@"+num;
+    }
+
+    public String captureScreen(String tname) {
+
+        String timeStamp = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+
+        TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+        File sourceFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
+
+        String targetFilePath=System.getProperty("user.dir")+"\\screenshots\\" + tname + "_" + timeStamp + ".png";
+        File targetFile=new File(targetFilePath);
+
+        sourceFile.renameTo(targetFile);
+
+        return targetFilePath;
+
     }
 }
