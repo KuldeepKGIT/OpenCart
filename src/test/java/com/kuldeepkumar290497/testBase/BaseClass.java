ace5b4068873d1dcc3988ec430b474873fdda7f7
KuldeepKGIT <kuldeepkumar290497@gmail.com>
2026-05-10 22:11:22 +0530

    5102026

--- src/test/java/com/kuldeepkumar290497/testBase/BaseClass.java
+++ src/test/java/com/kuldeepkumar290497/testBase/BaseClass.java
@@ -5,15 +5,19 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.*;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
@@ -37,18 +41,46 @@ public class BaseClass {
         p = new Properties();
         p.load(file);
 
+        if(p.getProperty("execution_env").equalsIgnoreCase("remote")){
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            //OS
+            if(os.equalsIgnoreCase("windows"))
+            {
+                capabilities.setPlatform(Platform.WIN10);
+            }
+            else if (os.equalsIgnoreCase("mac"))
+            {
+             capabilities.setPlatform((Platform.MAC));
+            }
+            else {
+                System.out.println("No matching os found!");
+                return;
+            }
+            //browser
+            switch(br.toLowerCase())
+            {
+                case "chrome": capabilities.setBrowserName("chrome"); break;
+                case "edge": capabilities.setBrowserName("MicrosoftEdge"); break;
+                default: System.out.println("No matching browser"); return;
+            }
 
-        switch (br.toLowerCase()){
-            case "edge":{
-                driver = new EdgeDriver();
-                break;
+            driver=new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),capabilities);
+        }
+
+
+        if(p.getProperty("execution_env").equalsIgnoreCase("local")){
+            switch (br.toLowerCase()){
+                case "edge":{
+                    driver = new EdgeDriver();
+                    break;
+                }
+                case "chrome":{
+                    driver = new ChromeDriver();
+                    break;
+                }
+                default:
+                    System.out.println("No Matching browser found!");
             }
-            case "chrome":{
-                driver = new ChromeDriver();
-                break;
-            }
-            default:
-                System.out.println("No Matching browser found!");
         }
 
         driver.manage().deleteAllCookies();
